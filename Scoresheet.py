44a4bd23a688cd485a1157b7c5d8811188bdc1b7
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-03-06 15:04:47 -0500

    Laid out structure per question

--- Scoresheet.py
+++ Scoresheet.py
@@ -63,6 +63,39 @@ class Scoresheet(QWidget):
 
         self.createDropDown(7, self.q7Label, self.q7Point)
 
+        #Question 8
+        self.q8Label = QLabel("08", self)
+
+        #Question 9
+
+        #Question 10
+
+        #Question 11
+
+        #Question 12
+
+        #Question 13
+
+        #Question 14
+
+        #Question 15
+
+        #Question 16
+
+        #Question 17
+
+        #Question 18
+
+        #Question 19
+
+        #Question 20
+
+        #OT 1
+
+        #OT 2
+
+        #OT 3
+
         self.scoreBox.setLayout(self.scoreLayout)
     
     def createDropDown(self, position, qLabel, qPoint=None):
